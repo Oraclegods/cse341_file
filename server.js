2c965e0f5248fb73d070f3cf48bfec294f7c8a89
oraclegods <folufolusho@gmail.com>
2025-06-02 12:11:41 +0100

    installed swagger

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
+const swaggerRoutes = require('./routes/swagger');
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+})
+
+app.use('/', swaggerRoutes);
 app.use('/project1', require('./routes/users'));
+// # getting hello world
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 mongodb.initDb((err) => {
   if (err) {
